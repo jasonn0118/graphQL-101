cc4529d7d34c96efc1c9d3633068498e1683686f
jasonn0118 <tlswoals2006@gmail.com>
2020-07-13 20:46:21 -0700

    initial set up

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql').graphqlHTTP;
+const { buildSchema } = require('graphql'); 
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/graphql', graphqlHttp({
+    // Query: get data
+    // Mutation: create, edit, remove data
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return [
+                'Romantic Cooking', 'Sailing', 'All-Night Coding'
+            ];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
+
+app.listen(3000);
